8a4350666c8419031b9591d6b52ac8f696c860c8
safridwirizky <safridwirizky@gmail.com>
2026-04-28 22:02:23 +0800

    fix: add welcoming greet in codespaces

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -12,8 +12,3 @@ local:
 PROFILE_EOF
 
 python examples/duckdb_to_file/scripts/init_db.py
-
-echo
-echo "✓ Playground ready. Try: cd examples/duckdb_to_file && drt run --select export_csv"
-echo
-echo "Or try: cd examples/duckdb_to_rest && bash setup.sh && drt run --select post_api"
